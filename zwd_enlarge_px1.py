31201403f982db2a9d538962458d19a1ccca90ec
9touchong <jackal12@qq.com>
2016-11-03 13:06:34 +0800

    Add files via upload

--- zwd_enlarge_px1.py
+++ zwd_enlarge_px1.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 #-------------------------------------------------------------
 #放大一个图层上的像素并放到新建图层上
 #放大标准 是增大几圈
@@ -6,13 +7,32 @@
 #            。-》。。-》。。。-》。。。。
 #                        。。。   。。。。
 #                                 。。。。
-#
-#
-#
-#
-#
-#
 #-------------------------------------------------------------
 from gimpfu import *
 
-def enlarge_pxs(img, layer):
+def enlarge_pxs(image, layer,t_num):
+	'''
+	Parameters:
+		image : image The current image.
+		layer : layer The layer of the image that is selected.
+		t_num : enlarge num
+	'''
+	gimp.message(t_num)
+
+register(
+	"python_fu_zwd_enlarge_pxs",
+	"放大像素",
+	"在新图层上放大原图层所有像素",
+	"zwd",
+	"Open source (BSD 3-clause license)",
+	"2013",
+	"<Image>/Filters/zwd/enlarge pxs",
+	"*",
+	[
+		(PF_STRING, "t_num", "只能输入整数",2)
+	],
+	[],
+	enlarge_pxs
+)
+
+main()
